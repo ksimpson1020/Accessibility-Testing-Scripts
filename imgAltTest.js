88c6766aaec9f0c636d4e6aca72d8352acf85afe
K Simpson <vsd911@gmail.com>
2022-08-24 12:19:26 -0500

    Adding no comment version

--- imgAltTest.js
+++ imgAltTest.js
@@ -12,3 +12,18 @@ result.push(images[i]);
 }
 }
 console.log(result)
+
+
+//no comment version for test parameter
+
+let images = document.querySelectorAll('img, i');
+let result = [];
+for (let i = 0; i < images.length; i++){
+if (images[i].hasAttribute('alt')){
+	console.log(images[i].getAttribute("alt"));
+} else {
+images[i].style.border= "2px solid #FF0000";
+result.push(images[i]);
+}
+}
+console.log(result)
